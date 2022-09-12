80d34513fea2aa94a133352aeb99e60fe2e231d2
Jeffrey <jeffrey.graham600@gmail.com>
2022-09-12 18:09:22 -0400

    function to create grid

--- src/Graph/Grid.tsx
+++ src/Graph/Grid.tsx
@@ -4,8 +4,20 @@ export default function Grid() {
   const [grid, setGrid] = useState([]);
 
   const makeGrid = () => {
-    
+    let g: any = []
+
+    for(let row = 0; row < 15; row += 1) {
+      let currentRow: any = [];
+      for(let col = 0; col < 50; col += 1){
+        currentRow.push([])
+      }
+      g.push(currentRow);
+    }
+
+    setGrid(g);
   };
+
+  console.log(grid);
 
   return <div>hello world</div>;
 }
