1ecee6edb930f874330b1fcd6372e9f29ab9bfb7
Jeffrey <jeffrey.graham600@gmail.com>
2022-09-12 23:58:36 -0400

    Made Logic to set a startNode onClick

--- src/Graph/Grid.tsx
+++ src/Graph/Grid.tsx
@@ -4,8 +4,10 @@ import "./Grid.css";
 
 export default function Grid() {
   const [grid, setGrid] = useState([]);
+  const [startOrFinish, setStartOrFinish] = useState(true);
+  const [startNode, setSNode] = useState([]);
 
-  useEffect(() => makeGrid(), []);
+  useEffect(() => makeGrid(), [startNode]);
 
   const makeGrid = () => {
     let g: any = [];
@@ -14,17 +16,20 @@ export default function Grid() {
       col: 0,
       isStart: false,
       isFinish: false,
+      gridCord: "",
     };
 
     for (let row = 0; row < 15; row += 1) {
       let currentRow: any = [];
       for (let col = 0; col < 50; col += 1) {
+        const [startR, startC] = startNode;
         currentRow.push(
           (node = {
             row: row,
             col: col,
-            isStart: false,
+            isStart: startR === row && startC === col ? true : false,
             isFinish: false,
+            gridCord: `${row},${col}`,
           })
         );
       }
@@ -34,29 +39,55 @@ export default function Grid() {
     setGrid(g);
   };
 
-  console.log(grid);
+  const setStartNode = (e: any) => {
+    const id = e.currentTarget.id;
+    let current = null;
+    let gridId = id.split(",").map((x: any) => Number(x));
+    let [r, c] = gridId;
+
+    for (let row of grid) {
+      for (let col of row) {
+        console.log(col);
+        if (col.col === c && col.row === r) {
+          col = { ...col, isStart: true };
+          setSNode(gridId);
+        }
+      }
+    }
+  };
+
+  const SorF = () => {
+    setStartOrFinish((currentStartOrFinish) => !currentStartOrFinish);
+  };
 
   return (
-    <div className="grid">
-      {grid.map((row: any, rowIndex) => {
-        return (
-          <div key={rowIndex}>
-            {row.map((node: any, nodeIndex: any) => {
-              let { row, col, isStart, isFinish } = node;
-              return (
-                <Node
-                  n={node}
-                  key={nodeIndex}
-                  row={row}
-                  col={col}
-                  isStart={isStart}
-                  isFinish={isFinish}
-                />
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="grid">
+        {grid.map((row: any, rowIndex) => {
+          return (
+            <div key={rowIndex}>
+              {row.map((node: any, nodeIndex: any) => {
+                let { row, col, isStart, isFinish, gridCord } = node;
+                return (
+                  <Node
+                    n={node}
+                    key={nodeIndex}
+                    row={row}
+                    col={col}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                    gridCord={gridCord}
+                    click={setStartNode}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
+        <button onClick={SorF}>
+          {startOrFinish ? "StartNode" : "EndNode"}
+        </button>
+      </div>
+    </>
   );
 }
--- src/Node/Node.tsx
+++ src/Node/Node.tsx
@@ -1,11 +1,14 @@
 import "./Node.css";
 
 export default function Node(props: any) {
-  let { row, col, isStart, isFinish, n } = props;
-  if (row === 0 && col === 0) isStart = true;
-  if (row === 14 && col === 49) isFinish = true;
+  let { row, col, isStart, isFinish, n, gridCord, click } = props;
   let startFinishNodes = isStart === true ? "Start" : isFinish ? "Finish" : "";
 
-  if (row === 1 && col === 0) isStart = true;
-  return <div className={`node${startFinishNodes}`}></div>;
+  return (
+    <div
+      onClick={click}
+      className={`node${startFinishNodes}`}
+      id={`${gridCord}`}
+    ></div>
+  );
 }
